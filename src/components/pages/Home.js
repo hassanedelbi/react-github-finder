c7feb44d034155bbea6420b760be491de78914b7
hassanedelbi <hassan.edelbi@gmail.com>
2022-01-27 13:55:58 +0200

    start adding context-reducer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,66 @@
-import React from 'react';
-
+import axios from 'axios';
+import { useState } from 'react';
+import Search from '../users/Search';
+import Alert from '../layout/Alert';
+import UserList from '../users/UserList';
 const Home = () => {
-	return <div>Home</div>;
+	const [loading, setLoading] = useState(false);
+	const [users, setUsers] = useState([]);
+	const [alert, setAlert] = useState(null);
+	const [user, setUser] = useState([]);
+
+	const searchUsers = async (enteredSearch) => {
+		setLoading(true);
+		const client_id = 'd9308aacf8b204d361fd';
+		const client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+		axios
+			.get(
+				`https://api.github.com/search/users?q=${enteredSearch}&client_id=${client_id}&client_secret=${client_secret}`
+			)
+			.then((res) => {
+				setUsers(res.data.items);
+				setLoading(false);
+			});
+	};
+
+	const getUser = async (username) => {
+		setLoading(true);
+		const client_id = 'd9308aacf8b204d361fd';
+		const client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+		setLoading(true);
+		axios
+			.get(
+				`https://api.github.com/search/users/${username}&client_id=${client_id}&client_secret=${client_secret}`
+			)
+			.then((res) => {
+				setUser(res.data);
+				setLoading(false);
+			});
+	};
+
+	const clearUsers = () => {
+		setUsers([]);
+		setLoading(false);
+	};
+
+	const setAlertMessage = (msg, type) => {
+		setAlert({ msg: msg, type: type });
+		setTimeout(() => {
+			setAlert(null);
+		}, 3000);
+	};
+	return (
+		<>
+			{alert !== null && <Alert alert={alert} />}
+			<Search
+				searchUsers={searchUsers}
+				clearUsers={clearUsers}
+				showClear={users.length > 0 ? true : false}
+				setAlert={setAlertMessage}
+			/>
+			<UserList loading={loading} users={users} />
+		</>
+	);
 };
 
 export default Home;
